bf24b45209f9c4238d32d00b7bb208fa0db58854
Weslleyolli <weslleyrichardson.cg@hotmail.com>
2023-09-28 14:41:20 -0300

    files not used

--- src/pages/Details/index.jsx
+++ src/pages/Details/index.jsx
@@ -1,9 +1,8 @@
-import { Container, Links, Content } from "./styles.js";
+import { Container, Content } from "./styles.js";
 
 import { Button } from "../../components/Button";
 import { Header } from "../../components/Header";
 import { Section } from "../../components/Section";
-import { Tag } from "../../components/Tag";
 import { ButtonText } from "../../components/ButtonText";
 
 export function Details() {
