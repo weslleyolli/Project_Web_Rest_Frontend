7f3e947b0760e3337564cbd0203d65ad1599e824
Weslleyolli <weslleyrichardson.cg@hotmail.com>
2023-10-05 14:27:40 -0300

    spacing into itens

--- src/pages/New/styles.js
+++ src/pages/New/styles.js
@@ -52,7 +52,7 @@ export const Form = styled.form`
             width: 100%;
             display: flex;
             align-items: center;
-            justify-content: space-around;
+            gap: 10px;
 
             div {
                 width: 35%;
