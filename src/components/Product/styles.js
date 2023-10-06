381a390fd6fc97ecbe11a0088ea09203f29c5891
weslleyolli <weslleyrichardson.cg@hotmail.com>
2023-10-05 23:43:29 -0300

    styled: position percent down

--- src/components/Product/styles.js
+++ src/components/Product/styles.js
@@ -111,8 +111,8 @@ export const ContainerPromotions = styled.button`
 
     > h8 {
         position: absolute;
-        top: -100px;
-        right: 20px;
+        top: -75px;
+        right: 35px;
         font-size: 10px;
     }
 
